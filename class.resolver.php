aecff805211805ac33d27c73293b577e65d397ce
pianogen <pianogen@students.zhaw.ch>
2013-05-15 22:19:25 +0200

    commit

---
+++ class.resolver.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * lib resolver
+ */
+class resolver {
+	
+	public $get;
+	
+	public $post;
+	
+	public function __construct( $ctrl = false, $action = false ) {
+		//set the request parameters
+		$this->get = &$_GET;
+		$this->post = &$_POST;
+		//for redirecting from a controller
+		if( $ctrl && $action ){
+			header("Location: index.php?ctrl=$ctrl&action=$action");
+			exit(0);
+		}
+	}
+	
+	public function handleRequest() {
+		//Startpage if controler or action is not set
+		if(!isset($this->get['ctrl'])) {
+			header("Location: index.php");
+			exit(0);
+		}
+	if (!isset($this->get['action'])) {
+			include_once "view\".$this->get['ctrl']";
+		}
+		//include the model, create an instance and push it to the controller
+		//more error handling is needed
+		include_once 'mdl/class.' . $this->get['ctrl'] . '.php';
+		$mdl_name = 'mdl_' . $this->get['ctrl'];
+		$mdl = new $mdl_name( $this );
+		//include the controller, create an instance and execute the action
+		//more error handling is needed
+		include_once 'ctrl/' . $this->get['ctrl'] . '.php';
+		$ctrl = new $this->get['ctrl']( $mdl, $this );
+		$action = $this->get['action'];
+		//now the action can be executed
+		$ctrl->$action();
+	}
+}
+?>
+
--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php phpinfo(); ?>
+<?php 
+include_once 'class.resolver.php';
+$resolver = new resolver();
+?>
+<html>
+	<head>
+		<title>WSUS Auswertung</title>
+		<style type="text/css">
+		<!-- 
+		  #nav { float: left; width: 200px; height: 800px; position:absolute; top:0px; left:0px; background:#527DE5; background-size:100% }
+		 #content { float: left; width:1000px; position:absolute; top:0px; left:200px; }
+		 #body { background-image:url(body.jpg); background-size: 800px 1200px; }
+		 -->
+		 </style>
+	</head>
+	<body>
+		<div id="nav">
+			<div style="padding: 20px 0px 0px 10px;">
+				<a href="index.php?ctrl=search" style="color:white; text-decoration:none; padding: 20px 0px 0px 10px;">Suchen</a>
+			</div>
+			<div style="padding: 20px 0px 0px 10px;">
+				<a href="index.php?ctrl=updates&action=list" style="color:white; text-decoration:none; padding: 20px 0px 0px 10px;">Update Management</a>
+			</div>
+			<div style="padding: 20px 0px 0px 10px;">
+				<a href="index.php?ctrl=packages&action=list" style="color:white; text-decoration:none; padding: 20px 0px 0px 10px;">Package Management</a>
+			</div>
+		</div>
+		<div id="content">
+			<?php 
+				if(!isset($_GET['ctrl']))
+            		include_once 'view/start.php';
+            	else 
+            		$resolver->handleRequest();
+			?>
+		</div>
+	</body>
+</html>
